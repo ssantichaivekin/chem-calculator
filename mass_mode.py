5805c17dfca9728f0abee5c3034cafa651b8437f
S.Santi Macbook <s.santichaivekin@gmail.com>
2018-02-09 23:50:53 -0800

    writing mass mode | uses csv

---
+++ mass_mode.py
@@ -0,0 +1,17 @@
+'''
+The mass mode function sets the element variables to be its mass.
+For example, H = 1, He = 4, Li = 3.
+We now have an inaccurate mass mode. We will implement a wikipedia
+web-scrape mass mode later.
+'''
+
+def read_mass_from_file() :
+    
+
+def mass_mode(element_dict) :
+
+
+
+
+
+def web_scrape_mass() :
