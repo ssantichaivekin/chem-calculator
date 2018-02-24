c85590b3214d3ca0438592058a372c732cf1b69c
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-02-24 00:57:03 -0800

    Move number matchers out of wiki_scrape.py into num_matcher.py

--- scraper/wiki_scraper.py
+++ scraper/wiki_scraper.py
@@ -1,12 +1,12 @@
 '''
-This file defines a wikipedia .
+This file defines wikipedia webscraping functionalities.
 '''
 
 import requests
 import re
 from bs4 import BeautifulSoup
 from read_write.file_writer import write_dict
-
+from matcher.num_matcher import parse_float_or_int, parse_num_with_units
 
 def wikisearch(query) :
     '''
@@ -22,79 +22,6 @@ def soupsite(siteurl) :
     '''
     r = requests.get(siteurl)
     return BeautifulSoup(r.text, 'lxml')
-
-def wikiscrape() :
-    return
-
-pattern_float = r'(?:−|-)?[0-9]+\.[0-9]+'
-float_regex = re.compile('(%s)' % (pattern_float))
-
-pattern_int = r'(?:−|-)?[0-9]+'
-int_regex = re.compile('(%s)' % (pattern_int))
-
-pattern_float_or_int = '(?:%s)|(?:%s)' % (pattern_float, pattern_int)
-
-def parse_float(info) :
-    '''
-    Find the first floating point value in the string. (Floating point values
-    are defined by the decimal point.)
-    Becareful! The minus (-) sign in wikipedia is not the same as in keyboard!
-    I don't know why this is the case. I will ask people later.
-    '''
-    matchobj = float_regex.search(info)
-    if matchobj :
-        num = matchobj.group(1).replace('−', '-')
-        res = float(num)
-        return res
-
-
-def parse_int(info) :
-    '''
-    Find the first integer in a string. But you return a float.
-    Quite counter intuitive. Maybe I should change how this works.
-    # TODO: Change how this works?
-    '''
-    matchobj = int_regex.search(info)
-    if matchobj :
-        num = matchobj.group(1).replace('−', '-')
-        res = float(num)
-        return res
-
-
-def parse_float_or_int(info) :
-    '''
-    Parse float. If there's no float, parse int.
-    '''
-    x = parse_float(info)
-    if x :
-        return x
-    return parse_int(info)
-
-# def get_unit_pattern(units) :
-#     units_pattern = '(?:'
-#     units_pattern += re.escape(units[0])
-#     for unit in units[1:] :
-#         units_pattern += '|' + re.escape(unit)
-#     units_pattern += ')'
-#     return units_pattern
-
-def parse_num_with_units(info, units) :
-    '''
-    Parse the number that comes just before any unit in
-    units.
-    # TODO : This may be buggy if the units have symbols like '-'
-    '''
-    for unit in units :
-        pattern_unit = re.escape(unit)
-        # print(info)
-        # print(pattern_unit, unit)
-        pattern_parse = r'(%s)(?:\s*)(?:%s)' % (pattern_float_or_int, pattern_unit)
-        parse_regex = re.compile(pattern_parse)
-        matchobj = parse_regex.search(info)
-        if matchobj :
-            numstr = matchobj.group(1)
-            return parse_float_or_int(numstr), unit
-
 
 def scrape_all_elements() :
     '''
