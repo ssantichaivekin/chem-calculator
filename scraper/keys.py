0cd819abc7f53498651946f7af339649b7493cfa
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-03-07 11:43:00 -0800

    Add a standard conversion unit for enthalpy.

--- scraper/keys.py
+++ scraper/keys.py
@@ -3,6 +3,9 @@ enthalpy_units = ['kJ/mol', 'kJ mol−1', 'kJ·mol−1', 'MJ/mol', 'MJ mol−1']
 mass_units = ['g·mol−1']
 
 
-conversion_table = {'MJ/mol'    : ('kJ/mol', 1000),
+conversion_table = {'kJ/mol'    : ('kJ/mol', 1),
+                    'kJ mol−1'  : ('kJ/mol', 1),
+                    'kJ·mol−1'  : ('kJ/mol', 1),
+                    'MJ/mol'    : ('kJ/mol', 1000),
                     'MJ mol−1'  : ('kJ/mol', 1000)}
 
