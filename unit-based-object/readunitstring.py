6bf6788fd04149bd592b532d215bfbbcf15e083b
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-04-22 17:25:43 -0700

    Edit units pattern.

--- unit-based-object/readunitstring.py
+++ unit-based-object/readunitstring.py
@@ -19,13 +19,13 @@ Note that we will have something like :
 # It is also possible that a group has a () enclosing it. In that case,
 # We capture the whole thing and perfrom read string on it recursively.
 # Note that in that case, the numerator and the denominator will be inversed.
-group_pattern = r'[a-z]{1,3}'
+group_pattern = r'[a-zA-Z]{1,3}'
 free_connector_pattern = r'[\.\+\s·.]'
 div_connector_pattern = r'/'
-inv_pattern = r'−|-1' # Note that the two - symbols are not of equal value here!
+inv_pattern = r'−1|-1' # Note that the two - symbols are not of equal value here!
 
 # we also have a separate parenthesis patttern for recursive call:
-paren_pattern = r'(.*)'
+paren_pattern = r'\(.*\)'
 
 # Now we compile these patterns for further use!
 # If the second group or the forth group is match, then it is inverted
@@ -37,4 +37,7 @@ full_pattern = '(%s)?(%s)?(%s|%s)(%s)?' % (
     inv_pattern
 )
 
+print(full_pattern)
+
 def read_unitstring(unitstring) :
+    return
